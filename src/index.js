57b5cea147ad6b9834f2cb653cb2f21527fb70f0
parasmalhotra522@gmail.com <parasmalhotra522@gmail.com>
2023-09-05 01:15:30 -0400

    small bug fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import './index.scss';
-import { CartContextProvider } from './context/cart.context';
 import {store} from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
